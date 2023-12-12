3a77de547f0abbab87c0d34d6e15aec5f2d71538
maritocuate <mario300@gmail.com>
2023-12-12 20:32:47 +0700

    chore: keyboard event

--- src/components/ButtonsBar.tsx
+++ src/components/ButtonsBar.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEventHandler, useCallback, useEffect, useState } from 'react'
 import Button from './Button'
 
 interface ButtonsBarProps {
@@ -11,13 +12,39 @@ interface Project {
 }
 
 const ButtonsBar = ({ buttonsRef, projects }: ButtonsBarProps) => {
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(0)
+
+  const handleKeyDown: KeyboardEventHandler<HTMLDivElement> = useCallback(
+    e => {
+      switch (e.key) {
+        case 'Enter':
+          window.open(projects[selectedButtonIndex].url)
+          break
+        case 'ArrowDown':
+          selectedButtonIndex < projects.length - 1 &&
+            setSelectedButtonIndex(prev => prev + 1)
+          break
+        case 'ArrowUp':
+          selectedButtonIndex > 0 && setSelectedButtonIndex(prev => prev - 1)
+      }
+    },
+    [selectedButtonIndex]
+  )
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
   return (
     <div className="hidden" ref={buttonsRef}>
-      <div className="flex-col">
+      <div className="flex-col" onKeyDown={handleKeyDown}>
         {projects.map((project, index) => (
           <Button
             key={index}
-            selected={/* selectedButtonIndex ===  index*/ true}
+            selected={index === selectedButtonIndex}
             text={project.text}
             onClick={() => window.open(project.url)}
           />
